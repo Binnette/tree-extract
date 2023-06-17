ad7602a9d15ab8bef7883a44e5348c4b0f13aefa
Binnette <binnette@gmail.com>
2023-06-17 11:23:26 +0200

    Update datalists

--- datalist.js
+++ datalist.js
@@ -1,3 +1,8 @@
+/*  This script get top 100 genus and top 999 species from Osm TagInfo
+    And save the results in genus.json and species.json in data folder
+    Those files are binded to inputs genus/species in the web app,
+    so the user can have autocompletion when typing tree genus/species.
+*/
 const https = require('https');
 const fs = require('fs');
 
@@ -31,7 +36,13 @@ function getDataFromTagInfo(key, count) {
                 .map(value => value.charAt(0).toUpperCase() + value.slice(1).toLowerCase())
                 .filter((value, index, self) => self.indexOf(value) === index);
 
-            fs.writeFileSync(outFile, JSON.stringify(values));
+            fs.writeFile(outFile, JSON.stringify(values, null, 2), (err) => {
+                if (err) {
+                    console.error(`🔴 Error writing to file ${outFile}: ${err}`);
+                } else {
+                    console.log(`🟢 Successfully wrote to file ${outFile}`);
+                }
+            });
         });
 
     }).on("error", (err) => {
